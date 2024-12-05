93fb15e1ff2040111ff87da76fcefcf69f646e6d
zhaoxiaolin <xiaolin9915@gmail.com>
2024-12-05 10:26:38 +0800

    fix(test): 测试发布

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -3,7 +3,7 @@
 const utils = require('@driver-cli/utils');
 
 // console.log('lodash core', lodash.get({ a: '2333'}, 'a'));
-console.log('sadafa', utils())
+console.log('sadafa1231', utils())
 
 function core() {
   return 'Hello from core';
